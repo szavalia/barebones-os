f6c5365881a55312b587ec307f11dddc26ef5b66
vriera <57446063+vriera@users.noreply.github.com>
2020-10-15 21:56:15 -0300

    nuevas user interrupts ( no hace nada)
    sem init, wait y post

--- Kernel/user_interrupts.c
+++ Kernel/user_interrupts.c
@@ -68,10 +68,30 @@ int int80_handler( uint64_t stack_pointer){
         case 17:
             sys_exit();
             break;
+        case 18:
+            sys_sem_init();
+            break;
+        case 19:
+            sys_sem_wait();
+            break;
+        case 20:
+            sys_sem_post();
+            break;
+
     }
     return 1;
 }
 
+void sys_sem_init(){
+    return;
+}
+
+void sys_sem_wait(){
+    return;
+}
+void sys_sem_post(){
+    return;
+}
 void sys_write(){
     char * buffer = (char *) getR13();
     int size = getR15();
